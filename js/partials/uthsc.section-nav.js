c7359603d429fd6af2aae6786507822a96b54abe
gpspake <gpspake@gmail.com>
2015-12-30 15:10:19 -0600

    section navigation working copy
    this is just a checkpoint after a good bit of work
    tab navigation and screen readr functionality working
    transitions need to be smoothed out

--- js/partials/uthsc.section-nav.js
+++ js/partials/uthsc.section-nav.js
@@ -7,15 +7,16 @@
     // to the top of the screen on desktop
     // allows foundation's sticky class to be used elsewhere.
     $(document).ready(function() {
-        var stickyNavTop = $('.uthsc-section-nav').offset().top;
+
+        var stickyNavTop = $('#uthsc-navigation-alt').offset().top;
 
         var stickyNav = function(){
             var scrollTop = $(window).scrollTop();
 
             if (scrollTop > stickyNavTop) {
-                $('.uthsc-section-nav').addClass('uthsc-sticky');
+                $('#uthsc-navigation-alt').addClass('uthsc-sticky');
             } else {
-                $('.uthsc-section-nav').removeClass('uthsc-sticky');
+                $('#uthsc-navigation-alt').removeClass('uthsc-sticky');
             }
         };
 
@@ -25,5 +26,48 @@
             stickyNav();
         });
     });
+
+    var openTimer;
+    var closeTimer;
+    var endTimer;
+
+    jQuery(".uthsc-navigation-column").bind('mouseover', openSubMenu);
+    jQuery("#uthsc-navigation-alt").bind('mouseleave', closeSubMenu);
+
+    function expandMenu() {
+        $('#uthsc-navigation-alt ul li ul').stop(true, true).slideDown(400);
+    }
+
+    function collapseMenu() {
+        $('#uthsc-navigation-alt ul li ul').stop(true, true).slideUp(400);
+    }
+
+    function clearTimer() {
+        clearTimeout(closeTimer);
+        clearTimeout(openTimer);
+    }
+
+    function openSubMenu() {
+        //$('#uthsc-navigation-alt').addClass('uthsc-navigation-active');
+        $('#uthsc-navigation-alt').removeClass('hide-class');
+        clearTimer();
+        openTimer = setTimeout(expandMenu, 400);
+    }
+
+    function closeSubMenu() {
+        //$('#uthsc-navigation-alt').removeClass('uthsc-navigation-active');
+        $('#uthsc-navigation-alt').addClass('hide-class');
+        //clearTimer();
+        //closeTimer = setTimeout(collapseMenu, 400);
+    }
+
+    $("#uthsc-navigation-alt").on( 'keyup', function( e ) {
+        openSubMenu();
+    } );
+
+    $(document).ready(function () {
+        closeSubMenu();
+    });
+
 
 }(jQuery);
