b2227d122431cb0a442bc25423b461013ef947d1
gpspake <gpspake@gmail.com>
2016-01-19 12:31:30 -0600

    namespace off-canvas selectors

--- js/partials/push-menu.js
+++ js/partials/push-menu.js
@@ -39,8 +39,8 @@
     Menu.prototype.options = {
         wrapper: '#uthsc-off-canvas-wrapper', // The content wrapper
         type: 'slide-left',                   // The menu type
-        menuOpenerClass: '.c-button',         // The menu opener class names (i.e. the buttons)
-        maskId: '#c-mask'                     // The ID of the mask
+        menuOpenerClass: '.uthsc-off-canvas-button',         // The menu opener class names (i.e. the buttons)
+        maskId: '#uthsc-off-canvas-mask'                     // The ID of the mask
     };
 
     /**
@@ -50,8 +50,8 @@
         this.body = document.body;
         this.wrapper = document.querySelector(this.options.wrapper);
         this.mask = document.querySelector(this.options.maskId);
-        this.menu = document.querySelector('#c-menu--' + this.options.type);
-        this.closeBtn = this.menu.querySelector('.c-menu__close');
+        this.menu = document.querySelector('#uthsc-off-canvas-menu--' + this.options.type);
+        this.closeBtn = this.menu.querySelector('.uthsc-off-canvas-menu__close');
         this.menuOpeners = document.querySelectorAll(this.options.menuOpenerClass);
         this._initEvents();
     };
