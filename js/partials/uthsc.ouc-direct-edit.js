46a00e3870365719b10676839d0653cc047eaec2
Todd Barber <ctbarber@gmail.com>
2016-03-31 11:58:41 -0500

    deleted file

--- js/partials/uthsc.ouc-direct-edit.js
+++
@@ -1,9 +0,0 @@
-function directEdit() {
-    var link = document.getElementById("uthsc-de").parentNode.innerHTML;
-    document.getElementById("uthsc-de").parentNode.innerHTML = "";
-    document.getElementById("uthsc-direct-edit").innerHTML = link;
-}
-
-$(document).ready(function(){
-    directEdit();
-});
