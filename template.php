4c0a01cac98cb427fd93d8cbcd0b4a3d7dfa7651
gpspake <gpspake@gmail.com>
2016-04-21 12:58:01 -0500

    add orbit slider to template

--- template.php
+++ template.php
@@ -318,6 +318,43 @@
         <!--Content Row 1-->
         <div class="row">
             <div class="columns medium-9">
+
+
+                <!--************-->
+                <!--Orbit Slider-->
+                <!--************-->
+                <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit="data-orbit" >
+                    <ul class="orbit-container">
+                        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+                        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+                        <li class="is-active orbit-slide">
+                            <img class="orbit-image" src="-resources/2015/images/slider-1.jpg" alt="Space">
+                            <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
+                        </li>
+                        <li class="orbit-slide">
+                            <img class="orbit-image" src="-resources/2015/images/slider-2.jpg" alt="Space">
+                            <figcaption class="orbit-caption">Lets Rocket!</figcaption>
+                        </li>
+                        <li class="orbit-slide">
+                            <img class="orbit-image" src="-resources/2015/images/slider-3.jpg" alt="Space">
+                            <figcaption class="orbit-caption">Encapsulating</figcaption>
+                        </li>
+                        <li class="orbit-slide">
+                            <img class="orbit-image" src="-resources/2015/images/slider-4.jpg" alt="Space">
+                            <figcaption class="orbit-caption">Outta This World</figcaption>
+                        </li>
+                    </ul>
+                    <nav class="orbit-bullets">
+                        <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+                        <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+                        <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+                        <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
+                    </nav>
+                </div>
+                <!--*************-->
+                <!--/Orbit Slider-->
+                <!--*************-->
+
                 <h3><strong><em>Content</em></strong> <b><i>Row</i></b> 1</h3>
                 <p>This is a full width row with columns inside of it.</p>
                 <p>Thanks to<a href="http://fortawesome.github.io/Font-Awesome/">
