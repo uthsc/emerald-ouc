c42d00c52d09d697e9a50324ce013ca606cc428c
gpspake <gpspake@gmail.com>
2016-04-22 16:31:58 -0500

    homepage news default image
    show uthsc logo if no featured image provided. 
    initial refactor of story styles

--- js/partials/uthsc.homepage-news.js
+++ js/partials/uthsc.homepage-news.js
@@ -26,28 +26,41 @@ function parseNewsPosts() {
             titleVisibilityClass = '',
             imageAltText = ''
 
-        if (typeof posts[i]._embedded['https://api.w.org/featuredmedia'] !== 'undefined') {
-            featuredImageLink = posts[i]._embedded['https://api.w.org/featuredmedia'][0]['source_url'];
+        if (typeof posts[i]._embedded['wp:featuredmedia'] !== 'undefined') {
+            featuredImageLink = posts[i]._embedded['wp:featuredmedia'][0]['source_url'];
             featuredImageLink = featuredImageLink.replace('.jpg', '-300x300.jpg')
+        } else {
+            featuredImageLink = '-resources/2015/images/homepage-news-featured-image-place-holder.jpg';
         }
 
-
-        if (i > 0) {
+        if (i < 1) {
             imageVisibilityClass = 'large-5';
             titleVisibilityClass = 'medium-12 medium-uncentered large-7 large-push-1';
+
+            html += '<a href="' + postLink + '"><div class="row collapse">' +
+                '<div class="columns small-4 medium-12' + imageVisibilityClass + '"> ' +
+                '<figure> ' +
+                '<img width="300" height="300" src="' + featuredImageLink + '"' +
+                'class="attachment-thumbnail size-thumbnail wp-post-image" ' +
+                'alt="Radhakrishna Feature"' +
+                '</figure>' +
+                '</div>' +
+                '<div class="columns small-12 small-centered' + titleVisibilityClass + '">' +
+                '<p><span class="anchortext">' + postTitle +'</span></p></div> ' +
+                '</div></a>';
+
+        } else {
+            html += '<a href="' + postLink + '"><div class="row collapse">' +
+                '<figure style="width: 100px; float: left; margin-bottom: .25em; margin-right: .75em;"> ' +
+                '<img width="100" height="100" src="' + featuredImageLink + '"' +
+                'class="attachment-thumbnail size-thumbnail wp-post-image" ' +
+                'alt="Radhakrishna Feature">' +
+                '</figure>' +
+                '<p><span class="anchortext">' + postTitle +'</span></p>' +
+                '</div></a>';
         }
 
-        html += '<a href="' + postLink + '"><div class="row collapse">' +
-            '<div class="columns small-4 medium-12' + imageVisibilityClass + '"> ' +
-            '<figure> ' +
-            '<img width="300" height="300" src="' + featuredImageLink + '"' +
-            'class="attachment-thumbnail size-thumbnail wp-post-image" ' +
-            'alt="Radhakrishna Feature"' +
-            '</figure>' +
-            '</div>' +
-            '<div class="columns small-12 small-centered' + titleVisibilityClass + '">' +
-            '<p><span class="anchortext">' + postTitle +'</span></p></div> ' +
-            '</div></a>';
+
     }
 
     return html;
