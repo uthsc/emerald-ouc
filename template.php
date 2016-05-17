e4f3b5d8356b61dd3c74eda9507653746205bf04
gpspake <gpspake@gmail.com>
2016-05-17 10:28:37 -0500

    move breadcrumbs and section nav to emerald

--- template.php
+++ template.php
@@ -33,6 +33,7 @@
 })(window,document,'script','dataLayer','GTM-PR6VFZ');</script>
 <!-- End Google Tag Manager -->
 
+
 <a href="#main-content" class="show-on-focus">Skip to content</a>
 
 <!--******************-->
@@ -321,34 +322,34 @@
                 <!--************-->
                 <!--Orbit Slider-->
                 <!--************-->
-                <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit="data-orbit" >
-                    <ul class="orbit-container">
-                        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-                        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-                        <li class="is-active orbit-slide">
-                            <img class="orbit-image" src="-resources/2015/images/slider-1.jpg" alt="Space">
-                            <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
-                        </li>
-                        <li class="orbit-slide">
-                            <img class="orbit-image" src="-resources/2015/images/slider-2.jpg" alt="Space">
-                            <figcaption class="orbit-caption">Lets Rocket!</figcaption>
-                        </li>
-                        <li class="orbit-slide">
-                            <img class="orbit-image" src="-resources/2015/images/slider-3.jpg" alt="Space">
-                            <figcaption class="orbit-caption">Encapsulating</figcaption>
-                        </li>
-                        <li class="orbit-slide">
-                            <img class="orbit-image" src="-resources/2015/images/slider-4.jpg" alt="Space">
-                            <figcaption class="orbit-caption">Outta This World</figcaption>
-                        </li>
-                    </ul>
-                    <nav class="orbit-bullets">
-                        <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-                        <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-                        <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-                        <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-                    </nav>
-                </div>
+<!--                <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit="data-orbit" >-->
+<!--                    <ul class="orbit-container">-->
+<!--                        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>-->
+<!--                        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>-->
+<!--                        <li class="is-active orbit-slide">-->
+<!--                            <img class="orbit-image" src="-resources/2015/images/slider-1.jpg" alt="Space">-->
+<!--                            <figcaption class="orbit-caption">Space, the final frontier.</figcaption>-->
+<!--                        </li>-->
+<!--                        <li class="orbit-slide">-->
+<!--                            <img class="orbit-image" src="-resources/2015/images/slider-2.jpg" alt="Space">-->
+<!--                            <figcaption class="orbit-caption">Lets Rocket!</figcaption>-->
+<!--                        </li>-->
+<!--                        <li class="orbit-slide">-->
+<!--                            <img class="orbit-image" src="-resources/2015/images/slider-3.jpg" alt="Space">-->
+<!--                            <figcaption class="orbit-caption">Encapsulating</figcaption>-->
+<!--                        </li>-->
+<!--                        <li class="orbit-slide">-->
+<!--                            <img class="orbit-image" src="-resources/2015/images/slider-4.jpg" alt="Space">-->
+<!--                            <figcaption class="orbit-caption">Outta This World</figcaption>-->
+<!--                        </li>-->
+<!--                    </ul>-->
+<!--                    <nav class="orbit-bullets">-->
+<!--                        <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>-->
+<!--                        <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>-->
+<!--                        <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>-->
+<!--                        <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>-->
+<!--                    </nav>-->
+<!--                </div>-->
                 <!--*************-->
                 <!--/Orbit Slider-->
                 <!--*************-->
