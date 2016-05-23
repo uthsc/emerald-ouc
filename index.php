5c75f951473c7a1849e87c9b3a768598233fbeee
Nic Winn <nic.liamg@gmail.com>
2016-05-23 15:23:37 -0500

    removed people form search placeholder text

--- index.php
+++ index.php
@@ -466,7 +466,7 @@
                 <div class="columns callout">
                     <!--Search form-->
                     <div class="input-group">
-                        <input class="input-group-field" type="text" placeholder="Search People...">
+                        <input class="input-group-field" type="text" placeholder="Search ...">
                         <div class="input-group-button">
                             <input type="submit" class="button" value="Submit">
                         </div>
