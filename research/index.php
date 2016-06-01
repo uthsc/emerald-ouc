27d18f681932610106f4f30daa74dfbc2e6969a0
gpspake <gpspake@gmail.com>
2016-06-01 11:43:53 -0500

    update links

--- research/index.php
+++ research/index.php
@@ -190,7 +190,7 @@
         <div class="row">
             <ul class="breadcrumbs column">
                 <li>
-                    <a aria-label="go to UTHSC homepage" href="/">
+                    <a aria-label="go to UTHSC homepage" href="/redesign/">
                         <span class="uthsc-home-icon-breadcrumbs fa fa-home fa-2x"></span>
                     </a>
                     <span class="fa fa-chevron-right" aria-hidden="true"></span>
@@ -799,7 +799,7 @@
         <div class="dropdown-pane bottom" id="left-breadcrumbs-dropdown" data-dropdown="data-dropdown"
              data-v-offset="0" data-auto-focus="true">
             <ul class="uthsc-off-canvas-breadcrumbs-list">
-                <li><a href="index.php" title="Home">Back to the Homepage</a></li>
+                <li><a href="/redesign/" title="Home">Back to the Homepage</a></li>
                 <li><a href="#" title="Core Clerkship Directors">Office of Research</a></li>
                 <li class="disabled"><strong>Current Page</strong></li>
             </ul>
@@ -890,14 +890,14 @@
 
     <!--Breadcrumbs-->
     <div class="uthsc-off-canvas-breadcrumbs button-group">
-        <a href="index.php" class="button home-button"><span class="fa fa-home"></span></a>
+        <a href="/redesign/" class="button home-button"><span class="fa fa-home"></span></a>
         <button class="button breadcrumbs-button dropdown" type="button" data-toggle="right-breadcrumbs-dropdown">
             Breadcrumbs Back Home
         </button>
         <div class="dropdown-pane bottom" id="right-breadcrumbs-dropdown" data-dropdown="data-dropdown"
              data-v-offset="0" data-auto-focus="true">
             <ul class="uthsc-off-canvas-breadcrumbs-list">
-                <li><a href="index.php" title="Home">Back to the Homepage</a></li>
+                <li><a href="/redesign/" title="Home">Back to the Homepage</a></li>
                 <li><a href="#" title="College of Medicine">Office of Research</a></li>
                 <li class="disabled"><strong>Current Page</strong></li>
             </ul>
