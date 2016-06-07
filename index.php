b6c90e27afc77894afb0f40184dcae78a4311368
Nic Winn <nic.liamg@gmail.com>
2016-06-07 13:02:38 -0500

    removed facebook code

--- index.php
+++ index.php
@@ -26,30 +26,6 @@
 </head>
 <body class="homepage">
 
-<!--***************************-->
-<!--Facebook SDK for JavaScript-->
-<!--***************************-->
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : 'your-app-id',
-            xfbml      : true,
-            version    : 'v2.5'
-        });
-    };
-
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-<!--****************************-->
-<!--/Facebook SDK for JavaScript-->
-<!--****************************-->
-
 <!--******************-->
 <!--Google Tag Manager-->
 <!--******************-->
