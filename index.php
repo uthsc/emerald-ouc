7c032422437e6d16ec997aa2241b1abb34a6b9c4
gpspake <gpspake@gmail.com>
2016-06-07 10:24:01 -0500

    update homepage mission images
    increase image height

--- index.php
+++ index.php
@@ -525,7 +525,7 @@
             <!--  Mission Row-->
             <div class="row uthsc-row-space medium-up-2 large-up-4">
                 <div class="columns">
-                    <img src="-resources/2015/images/mission-academics.jpg" class="uthsc-mission-image">
+                    <img src="-resources/2015/images/homepage-academics.jpg" class="uthsc-mission-image">
                     <h2>Academics</h2>
                     <p>Since 1911, we have been training health care professionals through education, research, clinical care and public service. Today, our Colleges of Dentistry, Graduate Health Sciences, Health Professions , Medicine, Nursing and Pharmacy serve more than 3,000 students and 1,300 residents, post docs and fellows across four campuses.</p>
                     <ul>
@@ -538,7 +538,7 @@
                     <a class="button hollow small" href="/education/"><span class="fa fa-plus-circle"></span> Learn More...</a>
                 </div>
                 <div class="columns">
-                    <img src="-resources/2015/images/mission-research-homepage.jpg" class="uthsc-mission-image">
+                    <img src="-resources/2015/images/homepage-research.jpg" class="uthsc-mission-image">
                     <h2>Research</h2>
                     <p>UTHSC has a long and rich tradition of accomplishments in basic, clinical and translational research in a wide variety of disciplines focused on the health issues that impact our community, our region, the state of Tennessee and the world.</p>
                     <ul>
@@ -551,8 +551,8 @@
                     <a class="button hollow small" href="research/"><span class="fa fa-plus-circle"></span> Learn More...</a>
                 </div>
                 <div class="columns">
-                    <img src="-resources/2015/images/mission-clinical-care.jpg" class="uthsc-mission-image">
-                    <h2>Clinical Care</h2>
+                    <img src="-resources/2015/images/homepage-clinical-care.jpg" class="uthsc-mission-image">
+                    <h2>Clinical Care</H2>
                     <p>Our faculty and students provide health care to the community through our network of hospitals, practice groups, specialty care clinics, and mobile health care facilities.</p>
                     <ul>
                         <li><a href="/bcdd/">Boling Center</a></li>
@@ -564,7 +564,7 @@
                     <a class="button hollow small" href="/clinicalcare/"><span class="fa fa-plus-circle"></span> Learn More...</a>
                 </div>
                 <div class="columns">
-                    <img src="-resources/2015/images/mission-public-service.jpg" class="uthsc-mission-image">
+                    <img src="-resources/2015/images/homepage-public-service.jpg" class="uthsc-mission-image">
                     <h2>Public Service</h2>
                     <p>Public service is central to our mission. Every year, thousands of faculty, staff and volunteers across four campuses located in Memphis, Knoxville, Chattanooga and Nashville provide clinical care, health education and preventive care programs to the people of Tennessee.</p>
                     <ul>
