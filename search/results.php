b01977ddcd9c6c9fb5c2180c89b8d3d7e9914b4d
gpspake <gpspake@gmail.com>
2016-07-06 14:27:01 -0500

    add search results page

---
+++ search/results.php
@@ -0,0 +1,36 @@
+<?php
+
+$people_search = array(
+    'meta' => array(
+
+    ),
+    'campus' => array(
+        'gspake1' => array(
+            'id' => 'T100345345',
+            'name' => 'Spake, George P',
+            'phone' => '(901) 448-1888',
+            'dept' => 'Web Services'
+        ),
+        'jsmith47' => array(
+            'id' => 'T200378678',
+            'name' => 'Smith, John A',
+            'phone' => '(901) 448-8743',
+            'dept' => 'Unknown Inc.'
+        )
+
+    ),
+    'system' => array(
+        'nwinn1' => array(
+            'id' => 'T200378678',
+            'name' => 'Winn, Nic',
+            'phone' => '(901) 448-1777',
+            'dept' => 'ITS Web Services'
+        ),
+        'alincoln' => array(
+            'id' => 'T200378678',
+            'name' => 'Lincoln, Abe',
+            'phone' => '(901) 448-1543',
+            'dept' => 'Office of the President'
+        )
+    )
+);
