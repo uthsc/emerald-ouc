43ae84865cdd5ba81ffba2cb4b61795f97c03aa3
Todd Barber <ctbarber@gmail.com>
2016-07-20 09:02:41 -0500

    change email link to the transition site

--- uthsc-site-nav.php
+++ uthsc-site-nav.php
@@ -56,7 +56,7 @@
                     <li><a href="/map/">Maps</a></li>
                     <li><a href="https://utap.tennessee.edu/">MyUT</a></li>
                     <li><a href="https://news.uthsc.edu/">News</a></li>
-                    <li><a href="/email/">Webmail</a></li>
+                    <li><a href="/email/transition/">Webmail</a></li>
                 </ul>
             </li>
             <li><a href="/give/"><span aria-hidden="true" class="fa fa-gift"></span> Make a Gift</a></li>
