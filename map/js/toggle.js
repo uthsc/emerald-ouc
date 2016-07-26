6a29363d2c82aa901d118761f8ecbaff39fd75be
Nic Winn <nic.liamg@gmail.com>
2016-07-26 10:40:11 -0500

    code formatting

--- map/js/toggle.js
+++ map/js/toggle.js
@@ -1,26 +1,34 @@
-$(".button").click(function () {
-    $(this).toggleClass("markers_off");
+$( ".button" ).click(function() {
+    $( this ).toggleClass( "markers_off" );
 });
 
-$(".pkng").click(function () {
+$( ".pkng" ).click(function() {
     toggleLocations('pkng');
 });
-$(".bldg").click(function () {
+$( ".bldg" ).click(function() {
     toggleLocations('bldg');
 });
-$(".labs").click(function () {
+$( ".labs" ).click(function() {
     toggleLocations('labs');
 });
 
-$("#bldg_dropdown").click(function () {
-    $(".bldg a.button").toggleClass("markers_off");
+$( "#bldg_dropdown" ).click(function() {
+    $(".bldg a.button").toggleClass( "markers_off" );
     toggleLocations('bldg');
 });
-$("#pkng_dropdown").click(function () {
-    $(".pkng a.button").toggleClass("markers_off");
+$( "#pkng_dropdown" ).click(function() {
+    $(".pkng a.button").toggleClass( "markers_off" );
     toggleLocations('pkng');
 });
-$("#labs_dropdown").click(function () {
-    $(".labs a.button").toggleClass("markers_off");
+$( "#labs_dropdown" ).click(function() {
+    $(".labs a.button").toggleClass( "markers_off" );
     toggleLocations('labs');
+});
+
+$(document).foundation();
+$('.f-dropdown').click(function() {
+    if ($(this).hasClass('open')) {
+        $('span[data-dropdown="'+$(this).attr('id')+'"]').trigger('click');
+        alert("close");
+    }
 });
