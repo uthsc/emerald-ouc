5ea603918a375270081fc32b490ea88a7d314b3c
Nic Winn <nic.liamg@gmail.com>
2016-07-29 16:25:09 -0500

    trimmed down to three functions for new foundation switches

--- map/js/toggle.js
+++ map/js/toggle.js
@@ -1,34 +1,9 @@
-$( ".button" ).click(function() {
-    $( this ).toggleClass( "markers_off" );
-});
-
-$( ".pkng" ).click(function() {
-    toggleLocations('pkng');
-});
-$( ".bldg" ).click(function() {
+$( "#buildings-switch" ).click(function() {
     toggleLocations('bldg');
 });
-$( ".labs" ).click(function() {
-    toggleLocations('labs');
-});
-
-$( "#bldg_dropdown" ).click(function() {
-    $(".bldg a.button").toggleClass( "markers_off" );
-    toggleLocations('bldg');
-});
-$( "#pkng_dropdown" ).click(function() {
-    $(".pkng a.button").toggleClass( "markers_off" );
+$( "#parking-switch" ).click(function() {
     toggleLocations('pkng');
 });
-$( "#labs_dropdown" ).click(function() {
-    $(".labs a.button").toggleClass( "markers_off" );
+$( "#labs-switch" ).click(function() {
     toggleLocations('labs');
-});
-
-$(document).foundation();
-$('.f-dropdown').click(function() {
-    if ($(this).hasClass('open')) {
-        $('span[data-dropdown="'+$(this).attr('id')+'"]').trigger('click');
-        alert("close");
-    }
 });
