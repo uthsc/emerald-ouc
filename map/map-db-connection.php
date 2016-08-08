4afb52e8e5c12f949a0b9a55d0f25f2da215294b
Todd Barber <ctbarber@gmail.com>
2016-08-08 11:09:42 -0500

    PHP to version 5+ code

--- map/map-db-connection.php
+++ map/map-db-connection.php
@@ -2,7 +2,7 @@
 
 include ("w000_dbname.inc");
 //$database = "ORATST";
-$connection =	OCILogon("a932", $password, $database);
+$connection =	oci_connect("a932", $password, $database);
 
 $sql = "select T170_ID, T170_BLDG_NAME
 								FROM A932T170
@@ -10,14 +10,14 @@ $sql = "select T170_ID, T170_BLDG_NAME
 								AND T170_TYPE = 'bldg'
 								ORDER BY T170_BLDG_NAME";
 
-$stmt = OCIParse($connection, $sql);
+$stmt = oci_parse($connection, $sql);
 
-if (OCIExecute($stmt)) {
-    while (OCIFetch($stmt)){
+if (oci_execute($stmt)) {
+    while (oci_fetch($stmt)){
         $bldg_list .= "<li><a href=\"javascript:scrollToMarker('".OCIResult($stmt,"T170_ID")."')\")>".OCIResult($stmt,"T170_BLDG_NAME")."</a></li>";
     }
 }
-ocifreestatement($stmt);
+oci_free_statement($stmt);
 
 $sql = "select T170_ID, T170_BLDG_NAME
 								FROM A932T170
@@ -25,14 +25,14 @@ $sql = "select T170_ID, T170_BLDG_NAME
 								AND T170_TYPE = 'pkng'
 								ORDER BY T170_BLDG_NAME";
 
-$stmt = OCIParse($connection, $sql);
+$stmt = oci_parse($connection, $sql);
 
-if (OCIExecute($stmt)) {
-    while (OCIFetch($stmt)){
+if (oci_execute($stmt)) {
+    while (oci_fetch($stmt)){
         $pkng_list .= "<li><a href=\"javascript:scrollToMarker('".OCIResult($stmt,"T170_ID")."')\")>".OCIResult($stmt,"T170_BLDG_NAME")."</a></li>";
     }
 }
-ocifreestatement($stmt);
+oci_free_statement($stmt);
 
 $sql = "select T170_ID, T170_BLDG_NAME
 								FROM A932T170
